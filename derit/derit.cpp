521a6f52909ad2652a642cd2373aab0cbac8704e
Alinchicck <89205336606a@gmail.com>
2023-11-13 18:55:40 +0300

    Добавлен файл Header1.h

--- derit/derit.cpp
+++ derit/derit.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-	char hello[5][10];
-	for (int i = 0; i < 5; i++) {
+	char hello[6][10];
+	for (int i = 0; i < 6; i++) {
 		cin >> hello[i];
 	}
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < ; i++) {
 		bool o = false;
 		for (int j = 0; j < 10; j++) {
 			if (hello[i][j] == 'a') {
