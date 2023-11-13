65a4980bfbe6ec9864301e87b9ced72baaf22b06
Alinchicck <89205336606a@gmail.com>
2023-11-13 19:04:25 +0300

    Изменено кол-во

--- Header1.h
+++ Header1.h
@@ -1,14 +1,15 @@
-﻿#include<iostream>
+#pragma once
+#include<iostream>
 using namespace std;
 int main()
 {
-	char hello[6][15];
+	char hello[6][10];
 	for (int i = 0; i < 6; i++) {
 		cin >> hello[i];
 	}
 	for (int i = 0; i < 6; i++) {
 		bool o = false;
-		for (int j = 0; j < 15; j++) {
+		for (int j = 0; j < 10; j++) {
 			if (hello[i][j] == 'a') {
 				o = true;
 				cout << hello[i] << endl;
